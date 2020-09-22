43ab2e9f5658c46c0ed02fecab75c03d00b120ad
Sudip Sth <sudipstha08@gmail.com>
2020-09-22 20:34:57 +0545

    Updates

---
+++ precache-manifest.a1615bd34bf389f50323273e0f8f42a4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c360e20dcf0226253d2fdfbd0e4b9318",
+    "url": "/github.io/codepen-clone/index.html"
+  },
+  {
+    "revision": "c98ec6aa36113cb093fb",
+    "url": "/github.io/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "4e652e335a513aced988",
+    "url": "/github.io/codepen-clone/static/css/main.594f4e11.chunk.css"
+  },
+  {
+    "revision": "c98ec6aa36113cb093fb",
+    "url": "/github.io/codepen-clone/static/js/2.c695722c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/codepen-clone/static/js/2.c695722c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e652e335a513aced988",
+    "url": "/github.io/codepen-clone/static/js/main.d4ba2169.chunk.js"
+  },
+  {
+    "revision": "002d5df1e2345f78b587",
+    "url": "/github.io/codepen-clone/static/js/runtime-main.544d56cf.js"
+  },
+  {
+    "revision": "47ce9d1c87276dd11397396fe777e52f",
+    "url": "/github.io/codepen-clone/static/media/cloud.47ce9d1c.svg"
+  },
+  {
+    "revision": "8a128b275fe26fcb9e78228267a40f1a",
+    "url": "/github.io/codepen-clone/static/media/down-arrow.8a128b27.svg"
+  },
+  {
+    "revision": "d7f89524969d1faae0f232714a165845",
+    "url": "/github.io/codepen-clone/static/media/logo.d7f89524.svg"
+  },
+  {
+    "revision": "9d5c7a8c742a3fbf81b1e41b14ae8f78",
+    "url": "/github.io/codepen-clone/static/media/nerd.9d5c7a8c.svg"
+  },
+  {
+    "revision": "367027a8660c8c0e6665c012435b9a44",
+    "url": "/github.io/codepen-clone/static/media/pen.367027a8.svg"
+  },
+  {
+    "revision": "213528bf4e9e1359c268b865931c142c",
+    "url": "/github.io/codepen-clone/static/media/pinned.213528bf.svg"
+  },
+  {
+    "revision": "473a0fa210a98b0808dbbf43701a524a",
+    "url": "/github.io/codepen-clone/static/media/settings.473a0fa2.svg"
+  },
+  {
+    "revision": "a3f7296228073ef8bd75a57147adda64",
+    "url": "/github.io/codepen-clone/static/media/view-change.a3f72962.svg"
+  }
+]);
